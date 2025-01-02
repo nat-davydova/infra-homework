396282e5dca4b83b2c38758fe93f484b8edf50cf
Natalia Davydova <natti.davydova@gmail.com>
2025-01-02 20:02:59 +0300

    homework #1: cjs

--- build/loader.cjs
+++ build/loader.cjs
@@ -1,2 +1,14 @@
 const yaml = require('yaml');
 const fs = require('node:fs');
+
+// eslint-disable-next-line node/no-deprecated-api
+require.extensions['.yml'] = function (module, filename) {
+  const content = fs.readFileSync(filename, 'utf-8');
+
+  try {
+    module.exports = yaml.parse(content)
+  } catch (err) {
+    err.message = filename + ': ' + err.message
+    throw err
+  }
+}
