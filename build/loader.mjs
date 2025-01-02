e65d1f983fd9d3ac4ff316d8fa3c2a74ac976241
Natalia Davydova <natti.davydova@gmail.com>
2025-01-02 22:17:10 +0300

    homework #1: esm

--- build/loader.mjs
+++ build/loader.mjs
@@ -1,3 +1,27 @@
 import yaml from 'yaml';
 import path from 'node:path';
 import fs from 'node:fs/promises';
+
+export async function load (url, context, nextLoad) {
+  //url приходит вида file://users/davydova/project/someFile.txt
+
+  const extension = path.extname(url);
+
+  if(extension === '.yml') {
+    try {
+      const path = new URL(url).pathname;
+      const content = await fs.readFile(path, 'utf8');
+
+      return {
+        format: 'json',
+        shortCircuit: true,
+        source: JSON.stringify(yaml.parse(content))
+      }
+    } catch(err) {
+      err.message = `${url}: ${err.message}`;
+      throw err;
+    }
+  }
+
+  return nextLoad(url);
+}
